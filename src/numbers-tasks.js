1baefb1f7ca684d72b6a664314270605d44ad54c
Kittoru <mama190774@gmail.com>
2024-11-11 20:42:52 +0300

    feat: add answers for 14 tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -180,8 +180,10 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const n = 10 ** pow;
+  const ost = num % n;
+  return num - ost + Math.round(ost / n) * n;
 }
 
 /**
@@ -201,8 +203,14 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  let count = 0;
+  for (let i = 1; i <= n; i += 1) {
+    if (n % i === 0) {
+      count += 1;
+    }
+  }
+  return count === 2;
 }
 
 /**
@@ -220,8 +228,11 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  if (typeof +value === 'number' && +value) {
+    return +value;
+  }
+  return def;
 }
 
 /**
@@ -235,8 +246,8 @@ function toNumber(/* value, def */) {
  *   -2 => -8
  *   0  => 0
  */
-function getCube(/* num */) {
-  throw new Error('Not implemented');
+function getCube(num) {
+  return num ** 3;
 }
 
 /**
@@ -252,8 +263,9 @@ function getCube(/* num */) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index <= 1) return index;
+  return getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2);
 }
 
 /**
